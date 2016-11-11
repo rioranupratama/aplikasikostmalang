01343942ffdaaa58550d58b958b28d055918ba7e
rioranup <rioranu2@gmail.com>
2016-11-11 09:43:08 +0700

    Menambahkan spinner untuk Ukuran Kamar

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiir3031/aplikasikostmalang/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiir3031/aplikasikostmalang/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etNama;
     EditText etAsal;
     EditText etTelp;
+    Spinner spUkuran;
     Button bOk;
     TextView tvHasil;
 
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         etNama = (EditText) findViewById(R.id.editTextNama);
         etAsal = (EditText) findViewById(R.id.editTextAsal);
         etTelp = (EditText) findViewById(R.id.editTextTelp);
+        spUkuran = (Spinner) findViewById(R.id.spinnerUkuran);
         bOk = (Button) findViewById(R.id.buttonOk);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
@@ -43,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
             String nama = etNama.getText().toString();
             String asal = etAsal.getText().toString();
             String telp = etTelp.getText().toString();
+            String ukuran = spUkuran.getSelectedItem().toString();
+
 
             tvHasil.setText(" Nama\t:\t" + nama + "\n Asal\t\t:\t"
-                    + asal + "\n No. Telp.\t:\t" + telp);
+                    + asal + "\n No. Telp.\t:\t" + telp
+                    + "\n\n Memesan kamar ukuran " + ukuran);
         }
 
     }
