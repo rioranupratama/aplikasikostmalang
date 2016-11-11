54c4467f3b46988ababff50ad1f8627b24e856bb
rioranup <rioranu2@gmail.com>
2016-11-11 09:13:56 +0700

    Memberi EditText Nama, Asal, dan No. Telp. serta Button"OK" untuk menampilkan di TextView Hasil

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiir3031/aplikasikostmalang/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiir3031/aplikasikostmalang/MainActivity.java
@@ -2,12 +2,92 @@ package id.sch.smktelkom_mlg.tugas01.xiir3031.aplikasikostmalang;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    EditText etAsal;
+    EditText etTelp;
+    Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etAsal = (EditText) findViewById(R.id.editTextAsal);
+        etTelp = (EditText) findViewById(R.id.editTextTelp);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       doProcess();
+                                       etNama.setText("");
+                                       etAsal.setText("");
+                                       etTelp.setText("");
+                                   }
+                               }
+        );
+
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String asal = etAsal.getText().toString();
+            String telp = etTelp.getText().toString();
+
+            tvHasil.setText(" Nama\t:\t" + nama + "\n Asal\t\t:\t"
+                    + asal + "\n No. Telp.\t:\t" + telp);
+        }
+
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String asal = etAsal.getText().toString();
+        String telp = etTelp.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        if (asal.isEmpty()) {
+            etAsal.setError("Nama belum diisi");
+            valid = false;
+        } else if (asal.length() < 3) {
+            etAsal.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etAsal.setError(null);
+        }
+
+        if (telp.isEmpty()) {
+            etTelp.setError("Nomer telepon belum diisi");
+            valid = false;
+        } else if (telp.length() < 8) {
+            etTelp.setError("Nomer minimal 8 angka");
+            valid = false;
+        } else {
+            etTelp.setError(null);
+        }
+
+        return valid;
     }
 }
+
